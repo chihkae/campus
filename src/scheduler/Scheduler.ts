cc3f44e4f217b88be9dbb8d5efacaf1450587a00
Emma Paris <eparis@atlassian.com>
2019-11-09 23:03:40 -0800

    fixed scheduler

--- src/scheduler/Scheduler.ts
+++ src/scheduler/Scheduler.ts
@@ -14,7 +14,6 @@ export default class Scheduler implements IScheduler {
                 return 0;
             }
         });
-
         let roomsSorted: SchedRoom[] = rooms.sort( function (a, b) {
             let numSeatsA = a.rooms_seats;
             let numSeatsB = b.rooms_seats;
@@ -26,23 +25,37 @@ export default class Scheduler implements IScheduler {
             } else {
                 return 0;
             }
-
         });
         let timeSlotsAvailable: TimeSlot[] = ["MWF 0800-0900" , "MWF 0900-1000", "MWF 1000-1100", "MWF 1100-1200",
             "MWF 1200-1300" , "MWF 1300-1400" , "MWF 1400-1500" , "MWF 1500-1600" , "MWF 1600-1700" , "TR  0800-0930",
             "TR  0930-1100" , "TR  1100-1230" , "TR  1230-1400" , "TR  1400-1530" , "TR  1530-1700"];
+        let result = this.makeSchedule(sectionSorted, roomsSorted, timeSlotsAvailable);
+        return result;
+    }
 
+    private makeSchedule(sectionSorted: SchedSection[], roomsSorted: SchedRoom[],
+                         timeSlotsAvailable: TimeSlot[]): Array<[SchedRoom, SchedSection, TimeSlot]> {
         let result: Array<[SchedRoom, SchedSection, TimeSlot]> = [];
-
-        sectionSorted.forEach( function (section: SchedSection) {
-            let toAdd: [SchedRoom, SchedSection, TimeSlot]  = [undefined, undefined, undefined];
-            toAdd.push(roomsSorted[0]);
-            toAdd.push(section);
-            toAdd.push(timeSlotsAvailable[0]);
-            timeSlotsAvailable.pop();
-            roomsSorted.pop();
-            result.push(toAdd);
-        });
+        let count = 0;
+        for (const section of sectionSorted) {
+            if (roomsSorted.length === 0) {
+                break;
+            } else if (roomsSorted[0].rooms_seats < (section.courses_fail + section.courses_pass +
+                section.courses_audit)) {
+                break;
+            } else {
+                if (count === 15) {
+                    count = 0;
+                }
+                let toAdd: [SchedRoom, SchedSection, TimeSlot]  = [undefined, undefined, undefined];
+                toAdd.push(roomsSorted[0]);
+                toAdd.push(section);
+                toAdd.push(timeSlotsAvailable[count]);
+                roomsSorted.pop();
+                count++;
+                result.push(toAdd);
+            }
+        }
         return result;
     }
         // TODO Implement this
