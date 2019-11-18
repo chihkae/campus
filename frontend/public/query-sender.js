6206ce44eb147801f4e156a73db8292fa09378f5
Emma Paris <eparis@atlassian.com>
2019-11-17 16:39:51 -0800

    implemented sender

--- frontend/public/query-sender.js
+++ frontend/public/query-sender.js
@@ -4,9 +4,19 @@
  * @param query The query object
  * @returns {Promise} Promise that must be fulfilled if the Ajax request is successful and be rejected otherwise.
  */
-CampusExplorer.sendQuery = function(query) {
-    return new Promise(function(fulfill, reject) {
-        // TODO: implement!
-        console.log("CampusExplorer.sendQuery not implemented yet.");
+CampusExplorer.sendQuery = function (query) {
+    return new Promise(function (fulfill, reject) {
+        let xhttp = new XMLHttpRequest();
+        xhttp.open("POST", "http://localhost:4321/query", true);
+        xhttp.setRequestHeader("Content-type", "application/json");
+        xhttp.send(JSON.stringify(query));
+
+        xhttp.onreadystatechange = function () {
+            if(this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+                fulfill(CampusExplorer.renderResult(JSON.parse(xhttp.responseText)));
+            }else if(this.readyState === XMLHttpRequest.DONE && this.status >= 400){
+                reject(CampusExplorer.renderResult(xhttp.statusText));
+            }
+        }
     });
 };
